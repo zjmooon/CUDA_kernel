89d9f51e2a899257c6ef2d24c635d859e7c60856
jiaming <1903009055@qq.com>
2026-02-08 23:45:29 +0800

    Remove unused high-resolution moon image to optimize storage.

---
+++ bilateralFilter/bilateralFilter.cu
@@ -0,0 +1,307 @@
+#include "../common.h"
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+#include <npp.h>
+#include <cuda_runtime.h>
+
+#define NPP_ENABLE true
+#define CUDA_ENABLE true
+
+constexpr float valSigma = 25.0f;
+constexpr float posSigma = 5.0f;
+
+#define RADIUS 3
+#define BLOCK_SIZE 16
+#define KERNEL_SIZE (2 * RADIUS + 1)
+#define TILE_SHARED (BLOCK_SIZE - 1) + KERNEL_SIZE
+#define TILE_SHARED_8 (8 * BLOCK_SIZE - 1) + KERNEL_SIZE
+__constant__ float d_spatial_weights_const[KERNEL_SIZE * KERNEL_SIZE];
+
+template<const int SHARED_SIZE_W,
+         const int SHARED_SIZE_H, 
+         const int N_TILE,
+         const int CIN>
+__global__ void kConv2dDirect_1x8_Tiling(
+    const unsigned short* __restrict__ input,   // [Cin][H][W]
+    unsigned short* __restrict__ output,        // [Cout][OH][OW]
+    int Cin, int H, int W,
+    int Cout, int KH, int KW,
+    int OH, int OW,
+    float sigma_r // rang space (值域标准差)
+) 
+{
+    __shared__ unsigned short s_input[SHARED_SIZE_H][SHARED_SIZE_W];
+
+    const int tx = threadIdx.x;
+    const int ty = threadIdx.y;
+
+    const int out_x_0 = blockIdx.x * blockDim.x * N_TILE + tx;
+    const int out_x_1 = out_x_0 + blockDim.x;
+    const int out_x_2 = out_x_1 + blockDim.x;
+    const int out_x_3 = out_x_2 + blockDim.x;
+    const int out_x_4 = out_x_3 + blockDim.x;
+    const int out_x_5 = out_x_4 + blockDim.x;
+    const int out_x_6 = out_x_5 + blockDim.x;
+    const int out_x_7 = out_x_6 + blockDim.x;
+
+    const int out_y = blockIdx.y * blockDim.y + ty;
+    const int out_c = blockIdx.z;
+
+    // if (out_y >= OH || out_c >= Cout) return;  // 会与后续的__syncthreads() 构成死锁
+
+    const int in_start_y = blockIdx.y * blockDim.y;
+    const int in_start_x = blockIdx.x * blockDim.x * N_TILE;
+    
+    // 8个像素的累加器和权重归一化因子
+    float sum0 = 0, sum1 = 0 , sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0, sum6 = 0, sum7 = 0;
+    float w_sum0 = 0.0f, w_sum1 = 0.0f, w_sum2 = 0.0f, w_sum3 = 0.0f, 
+          w_sum4 = 0.0f, w_sum5 = 0.0f, w_sum6 = 0.0f, w_sum7 = 0.0f;
+    const float inv_rev_sigma2 = -1.0f / (2.0f * sigma_r * sigma_r);
+    
+    for (int c = 0; c < Cin; c++) {
+        // global to shared memory 
+        // 合并访存以及避免Bank Conflict
+        # pragma unroll
+        for (int y = ty; y < SHARED_SIZE_H; y += blockDim.y) {
+            int in_y = in_start_y + y;
+            # pragma unroll
+            for (int x = tx; x < SHARED_SIZE_W; x += blockDim.x) {
+                int in_x = in_start_x + x;
+            
+                s_input[y][x] = (in_x >= 0 && in_x < W && in_y >= 0 && in_y < H) ? 
+                    input[c * H * W + in_y * W + in_x] : 0; // 填充0
+            }
+        }
+        __syncthreads();
+
+        //  8个值域中心像素值
+        unsigned short center_vals0, center_vals1, center_vals2, center_vals3, center_vals4, center_vals5, center_vals6, center_vals7;
+        center_vals0 = s_input[ty][tx + 0 * blockDim.x];
+        center_vals1 = s_input[ty][tx + 1 * blockDim.x];
+        center_vals2 = s_input[ty][tx + 2 * blockDim.x];
+        center_vals3 = s_input[ty][tx + 3 * blockDim.x];
+        center_vals4 = s_input[ty][tx + 4 * blockDim.x];
+        center_vals5 = s_input[ty][tx + 5 * blockDim.x];
+        center_vals6 = s_input[ty][tx + 6 * blockDim.x];
+        center_vals7 = s_input[ty][tx + 7 * blockDim.x];
+        
+        // convolution compute
+        # pragma unroll
+        for (int ky = 0; ky < KH; ++ky) {
+            int shared_idx_y = ty + ky;
+            # pragma unroll
+            for (int kx = 0; kx < KW; ++kx) {
+                // 空域权重(预计算查表)
+                float s_weight = d_spatial_weights_const[out_c * Cin * KH * KW +
+                            c * KH * KW +
+                            ky * KW + kx];
+
+                // shared memory 索引映射
+                int shared_x0 = tx + kx;
+
+                unsigned short s0 = s_input[shared_idx_y][shared_x0];
+                unsigned short s1 = s_input[shared_idx_y][shared_x0 + blockDim.x];
+                unsigned short s2 = s_input[shared_idx_y][shared_x0 + blockDim.x * 2];
+                unsigned short s3 = s_input[shared_idx_y][shared_x0 + blockDim.x * 3];
+                unsigned short s4 = s_input[shared_idx_y][shared_x0 + blockDim.x * 4];
+                unsigned short s5 = s_input[shared_idx_y][shared_x0 + blockDim.x * 5];
+                unsigned short s6 = s_input[shared_idx_y][shared_x0 + blockDim.x * 6];
+                unsigned short s7 = s_input[shared_idx_y][shared_x0 + blockDim.x * 7];
+                
+                // 值域权重: exp(-|I(p)-I(q)|^2 / 2*sigma_r^2)
+                float diff0 = center_vals0 - s0;
+                float diff1 = center_vals1 - s1;
+                float diff2 = center_vals2 - s2;
+                float diff3 = center_vals3 - s3;      
+                float diff4 = center_vals4 - s4;
+                float diff5 = center_vals5 - s5;
+                float diff6 = center_vals6 - s6;
+                float diff7 = center_vals7 - s7;
+                
+                // 总权重
+                float r_weight, total_weight;
+
+                r_weight = expf(diff0 * diff0 * inv_rev_sigma2);    
+                total_weight = s_weight * r_weight;    
+                w_sum0 += total_weight;  
+                sum0 += s0 * total_weight;
+
+                r_weight = expf(diff1 * diff1 * inv_rev_sigma2);    
+                total_weight = s_weight * r_weight;    
+                w_sum1 += total_weight;  
+                sum1 += s1 * total_weight;
+
+                r_weight = expf(diff2 * diff2 * inv_rev_sigma2);    
+                total_weight = s_weight * r_weight;    
+                w_sum2 += total_weight;  
+                sum2 += s2 * total_weight;
+
+                r_weight = expf(diff3 * diff3 * inv_rev_sigma2);    
+                total_weight = s_weight * r_weight;    
+                w_sum3 += total_weight;  
+                sum3 += s3 * total_weight;
+
+                r_weight = expf(diff4 * diff4 * inv_rev_sigma2);    
+                total_weight = s_weight * r_weight;    
+                w_sum4 += total_weight;  
+                sum4 += s4 * total_weight;
+
+                r_weight = expf(diff5 * diff5 * inv_rev_sigma2);    
+                total_weight = s_weight * r_weight;    
+                w_sum5 += total_weight;  
+                sum5 += s5 * total_weight;
+
+                r_weight = expf(diff6 * diff6 * inv_rev_sigma2);    
+                total_weight = s_weight * r_weight;    
+                w_sum6 += total_weight;  
+                sum6 += s6 * total_weight;
+
+                r_weight = expf(diff7 * diff7 * inv_rev_sigma2);    
+                total_weight = s_weight * r_weight;    
+                w_sum7 += total_weight;  
+                sum7 += s7 * total_weight;
+
+            }
+        }
+        __syncthreads();
+    }
+
+    int base = out_c * OH * OW + out_y * OW;
+    if (out_x_0 < OW) output[base + out_x_0] = sum0 / (w_sum0 + 1e-6f);
+    if (out_x_1 < OW) output[base + out_x_1] = sum1 / (w_sum1 + 1e-6f);
+    if (out_x_2 < OW) output[base + out_x_2] = sum2 / (w_sum2 + 1e-6f);
+    if (out_x_3 < OW) output[base + out_x_3] = sum3 / (w_sum3 + 1e-6f);
+    if (out_x_4 < OW) output[base + out_x_4] = sum4 / (w_sum4 + 1e-6f);
+    if (out_x_5 < OW) output[base + out_x_5] = sum5 / (w_sum5 + 1e-6f);
+    if (out_x_6 < OW) output[base + out_x_6] = sum6 / (w_sum6 + 1e-6f);
+    if (out_x_7 < OW) output[base + out_x_7] = sum7 / (w_sum7 + 1e-6f);
+    
+}
+
+void iConv2dDirect_8_Tiling(
+    const unsigned short* __restrict__ input,   // [Cin][H][W]
+    /* kernel, */  // [Cout][Cin][KH][KW]
+    unsigned short* __restrict__ output,        // [Cout][OH][OW]
+    int Cin, int H, int W,
+    int Cout, int KH, int KW,
+    int OH, int OW,
+    float valSigma) 
+{
+    dim3 block(16, 16);
+    dim3 grid(CEIL(OW, block.x * 8), CEIL(OH, block.y), Cout);
+
+    kConv2dDirect_1x8_Tiling<TILE_SHARED_8, TILE_SHARED, 8, 1><<<grid, block>>>(
+        input, output,
+        Cin, H, W,
+        Cout, KH, KW,
+        OH, OW,
+        valSigma
+    );
+} 
+
+void generateSpatialWeight(int nRadius, float nPosSquareSigma, float* h_spatial_weights) {
+    int size = 2 * nRadius + 1;
+    
+    float twoSigmaSquare = 2.0f * nPosSquareSigma; // NPP 传入的通常已经是 sigma^2
+    
+    for (int y = -nRadius; y <= nRadius; ++y) {
+        for (int x = -nRadius; x <= nRadius; ++x) {
+            float distanceSq = static_cast<float>(x * x + y * y);
+            // 计算高斯分布
+            float weight = std::exp(-distanceSq / twoSigmaSquare);
+            
+            // 映射到数组索引 [0, 2*nRadius]
+            h_spatial_weights[(y + nRadius) * size + (x + nRadius)] = weight;
+        }
+    }
+}
+
+int main() {
+    // load CPU 16u img
+    int width, height;
+    cv::Mat img = cv::imread("asset/moon_3840_2160_16u.tif", cv::IMREAD_UNCHANGED);
+    if (img.empty()) { std::cerr << "Failed to load\n"; return -1; }
+    width = img.cols;
+    height = img.rows;
+
+    size_t imgSize = width * height * sizeof(unsigned short);
+
+    // alloc GPU memory
+    unsigned short* d_src_16u;
+    unsigned short* d_dst_16u;
+    CHECK(cudaMalloc(reinterpret_cast<void **>(&d_src_16u), height * width * sizeof(unsigned short)));
+    CHECK(cudaMalloc(reinterpret_cast<void **>(&d_dst_16u), height * width * sizeof(unsigned short)));
+    
+    // host -> device
+    cudaMemcpy(d_src_16u, img.data, imgSize, cudaMemcpyHostToDevice);
+
+    if (NPP_ENABLE) 
+    {
+        // set NPP params
+        NppiSize sizeROI = { width, height };
+
+        NppStatus status = nppiFilterBilateralGaussBorder_16u_C1R(
+            d_src_16u, width * sizeof(unsigned short),
+            sizeROI, {0,0},
+            d_dst_16u, width * sizeof(unsigned short),
+            sizeROI,
+            RADIUS,
+            1,
+            valSigma * valSigma,
+            posSigma * posSigma,
+            NPP_BORDER_REPLICATE
+        );
+
+        if (status != NPP_SUCCESS) {
+            std::cerr << "NPP bilateral filter failed: " << status << "\n";
+        }
+
+        std::vector<unsigned short> output(height * width);
+        cudaMemcpy(output.data(), d_dst_16u, imgSize, cudaMemcpyDeviceToHost);
+
+        // build a 1-channel 16-bit OpenCV Mat and save as 16-bit TIFF
+        cv::Mat outImg16u(height, width, CV_16UC1, output.data());
+        cv::imwrite("asset/moon_3840_2160_bilateraled_16u.tif", outImg16u);
+    }
+
+    if (CUDA_ENABLE) 
+    {
+        int repeat_times = 10;
+        float total_time;
+        // 计算卷积后输出尺寸
+        int OH = (height - KERNEL_SIZE) + 1;
+        int OW = (width - KERNEL_SIZE) + 1;
+
+        float* h_spatial_weights = (float*)malloc(KERNEL_SIZE * KERNEL_SIZE * sizeof(float)); 
+        generateSpatialWeight(RADIUS, posSigma * posSigma, h_spatial_weights);
+        cudaMemcpyToSymbol(d_spatial_weights_const, h_spatial_weights, KERNEL_SIZE * KERNEL_SIZE * sizeof(float));
+        // gpu N Tiling
+        CHECK(cudaMemset(d_dst_16u, 0, height * width * sizeof(unsigned short)));
+        total_time = TIME_RECORD(repeat_times, ([&]{
+            iConv2dDirect_8_Tiling(
+                d_src_16u,
+                d_dst_16u,
+                1, height, width,
+                1, KERNEL_SIZE, KERNEL_SIZE,
+                OH, OW, valSigma
+            );
+        }));
+        std::cout << GREEN << std::endl  << __FILE__ << ":" << __LINE__ << 
+        " [device 1x8" << " Tiling]: elapsed = " << total_time / repeat_times << " ms " << RESET << std::endl;
+        
+        std::vector<unsigned short> output(height * width);
+        cudaMemcpy(output.data(), d_dst_16u, imgSize, cudaMemcpyDeviceToHost);
+        CHECK(cudaMemcpy(output.data(), d_dst_16u, OH * OW * sizeof(unsigned short), cudaMemcpyDeviceToHost));
+        cv::Mat outImg16u(height, width, CV_16UC1, output.data());
+        cv::imwrite("asset/moon_3840_2160_bilateraled_tile_16u.tif", outImg16u);
+    }
+ 
+    // free GPU memory
+    cudaFree(d_src_16u);
+    cudaFree(d_dst_16u);
+
+    std::cout << "Done!\n";
+    return 0;
+}
